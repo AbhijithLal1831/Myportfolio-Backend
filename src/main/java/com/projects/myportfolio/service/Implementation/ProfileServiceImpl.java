df01d29b1b6036eda4cbb8060d2f7a89bfacfc25
Abhijith-Lal <abhijithlalharivaram@gmail.com>
2024-12-14 13:33:36 +0530

    Initial services

---
+++ src/main/java/com/projects/myportfolio/service/Implementation/ProfileServiceImpl.java
@@ -0,0 +1,13 @@
+package com.projects.myportfolio.service.Implementation;
+
+import com.projects.myportfolio.service.ProfileService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileServiceImpl implements ProfileService {
+
+}
---
+++ src/main/java/com/projects/myportfolio/service/ProfileService.java
@@ -0,0 +1,5 @@
+package com.projects.myportfolio.service;
+
+public interface ProfileService {
+
+}
